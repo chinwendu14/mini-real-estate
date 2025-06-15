684d29380be9821d386d4998b74199b4609cef16
chinwendu <chinwendususanna93@gmail.com>
2025-06-15 01:03:23 +0100

    update

---
+++ src/app/apartment/details/_component/detailcom.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { apartments } from "@/sharables/DummyData";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+const Detailcom = () => {
+  const params = useSearchParams();
+
+  const [id, setId] = useState<string | null>(null);
+
+  useEffect(() => {
+    setId(params.get("id"));
+  }, [params]);
+
+  const apt = apartments.find((a) => a.id === id);
+  return (
+    <div className=" p-3 min-h-screen flex flex-col items-center justify-center p-4">
+      <h4 className="text-2xl text-primary font-bold mb-4">
+        Apartment Layout Details
+      </h4>
+      <div className="bg-white rounded-xl shadow p-4">
+        <Image
+          src={`${apt?.thumbnail}`}
+          alt={`Layout`}
+          width={300}
+          height={128}
+          className="w-full max-w-md "
+        />
+
+        <div className="mt-4 text-lg">
+          <p>
+            <strong>Area:</strong> {apt?.area}
+          </p>
+          <p>
+            <strong>Type:</strong> {apt?.unitType}
+          </p>
+          <p>
+            <strong>Rooms:</strong> {apt?.rooms}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Detailcom;
--- src/app/apartment/details/page.tsx
+++ src/app/apartment/details/page.tsx
@@ -1,55 +1,15 @@
 "use client";
 export const dynamic = "force-dynamic";
 
-import { apartments } from "@/sharables/DummyData";
-import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-import React, { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
+import Detailcom from "./_component/detailcom";
 
 const Page = () => {
-  const params = useSearchParams();
-
-  // const id = useSearchParams();
-  // const aptId = id.get("id");
-
-  const [id, setId] = useState<string | null>(null);
-
-  useEffect(() => {
-    setId(params.get("id"));
-  }, [params]);
-
-  const apt = apartments.find((a) => a.id === id);
-
   // if (!apt) return <p>Apartment not found.</p>;
   return (
     <>
       <Suspense fallback={<p>Loading apartment details...</p>}>
-        <div className=" p-3 min-h-screen flex flex-col items-center justify-center p-4">
-          <h4 className="text-2xl text-primary font-bold mb-4">
-            Apartment Layout Details
-          </h4>
-          <div className="bg-white rounded-xl shadow p-4">
-            <Image
-              src={`${apt?.thumbnail}`}
-              alt={`Layout`}
-              width={300}
-              height={128}
-              className="w-full max-w-md "
-            />
-
-            <div className="mt-4 text-lg">
-              <p>
-                <strong>Area:</strong> {apt?.area}
-              </p>
-              <p>
-                <strong>Type:</strong> {apt?.unitType}
-              </p>
-              <p>
-                <strong>Rooms:</strong> {apt?.rooms}
-              </p>
-            </div>
-          </div>
-        </div>
+        <Detailcom />
       </Suspense>
     </>
   );
